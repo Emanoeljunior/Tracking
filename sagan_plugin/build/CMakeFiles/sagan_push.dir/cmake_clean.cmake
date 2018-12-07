de58e18b243f5597f552cefc5a1c43a444c5bf59
Emanoel Alves Junior <emanoelalvesjr@gmail.com>
2018-12-07 16:01:30 -0200

    update fail pose 000

---
+++ sagan_plugin/build/CMakeFiles/sagan_push.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sagan_push.dir/sagan_push.cc.o"
+  "libsagan_push.pdb"
+  "libsagan_push.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sagan_push.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
